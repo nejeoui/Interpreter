94a7f219f4099ac0656bc128427ca27057da141a
A. Nejeoui <a.nejeoui@gmail.com>
2019-10-11 02:23:40 +0100

    README.MD

--- src/test/java/com/nejeoui/interpreter/InterpreterApplicationTests.java
+++ src/test/java/com/nejeoui/interpreter/InterpreterApplicationTests.java
@@ -21,7 +21,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+
 @WebAppConfiguration
 @SpringBootTest(classes = InterpreterApplication.class)
 public class InterpreterApplicationTests {
